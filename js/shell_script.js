0fc934450cd4e0a3e32b4b493fb378722a8c221f
solitarysp <solitarysp@gmail.com>
2023-01-24 16:36:47 +0700

    add

--- js/shell_script.js
+++ js/shell_script.js
@@ -2,9 +2,20 @@ setInterval(bypassStartShell, 2000);
 
 async function bypassStartShell() {
   try {
-    document.getElementsByClassName('mat-checkbox-label')[0].click();
+    let button;
+    const spans = document.getElementsByTagName("span");
+    for (let i = 0; i < spans.length; i++) {
+      if (spans[i].textContent.trim() == 'Start Cloud Shell') {
+        button = spans[i];
+      }
+    }
+    if (button) {
+      document.getElementsByClassName('mat-checkbox-label')[0].click();
+      await sleep(500)
+      button.click();
+    }
   } catch (e) {
-
+    console.error(e)
   }
 }
 
@@ -12,12 +23,19 @@ setInterval(bypassRecheckShell, 2000);
 
 async function bypassRecheckShell() {
   try {
-    document.getElementsByClassName('mat-button-wrapper')[0].click();
+    let button;
+    const spans = document.getElementsByTagName("span");
+    for (let i = 0; i < spans.length; i++) {
+      if (spans[i].textContent.trim() == 'Continue') {
+        button = spans[i];
+      }
+    }
+    if (button) {
+      document.getElementsByClassName('mat-button-wrapper')[0].click();
+    }
   } catch (e) {
   }
 }
-
-
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
