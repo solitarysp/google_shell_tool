a8dcddaf2fb00212af83d3ff2402403fec8fdad6
solitarysp <solitarysp@gmail.com>
2023-02-15 19:12:04 +0700

    add

--- js/shell_script.js
+++ js/shell_script.js
@@ -38,6 +38,48 @@ async function bypassRecheckShell() {
   }
 }
 
+setInterval(reLoginGoogle, 2000);
+
+async function reLoginGoogle() {
+  try {
+    if (document.location.href.indexOf('signin/confirmidentifier') > 0) {
+        if(document.getElementById('headingText').textContent === 'Xác minh danh tính của bạn'){
+
+          let button;
+          const spans = document.getElementsByTagName("span");
+          for (let i = 0; i < spans.length; i++) {
+            if (spans[i].textContent.trim() == 'Tiếp theo') {
+              button = spans[i];
+            }
+          }
+          if (button) {
+            button.click();
+          }
+
+        }
+    }
+  } catch (e) {
+  }
+  try {
+    if (document.location.href.indexOf('signin/challenge/pwd') > 0) {
+      const inputs = document.getElementsByTagName("input")
+      for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].getAttribute('type') == 'password') {
+          inputs[i].value = "Chicanem1234";
+
+          const spans = document.getElementsByTagName("span");
+          for (let i = 0; i < spans.length; i++) {
+            if (spans[i].textContent.trim() == 'Tiếp theo') {
+                spans[i].click();;
+            }
+          }
+        }
+      }
+    }
+  } catch (e) {
+  }
+}
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
